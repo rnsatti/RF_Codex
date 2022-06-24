24307b6813abda030885664cb958ed194416311e
nsatti <nsatti@gmail.com>
2022-06-24 10:07:40 +0400

    Update run.sh

--- RF_Codex_Production_Stream/bin/run.sh
+++ RF_Codex_Production_Stream/bin/run.sh
@@ -30,7 +30,7 @@ cd /robot/RF_Codex/RF_Codex_$ENV'_'Stream/APP/ESHOP/Test_Case && robot $EXE_RESU
 #docker run -v "C:\du\Projects\QMS\TAM\2022\Codex\RF_Codex":"/results" -e PUSH_GATE=http://192.168.0.16:9091 -e CASES=Suite -e EXE_RESULTS="--outputdir /results/ResultFile/Result" -e SELECTED="--include QAD-11111" -e ENV=Production -e METRIC_COMPL=ESHOP -e JOB_NAME=ESHOP_Production --name rf_test rf_codex:1.0
 
 
-sh /ngt-codex-qa/ngt-codex-qa/bin/prometrics.sh
+sh /robot/bin/prometrics.sh
 #sh /ngt-codex-qa/ngt-codex-qa/bin/checkiotenv.sh
 
 
